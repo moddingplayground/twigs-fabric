8a4ef6cd9938d725dede2aad75601796a917a719
andantet <orrdaniel73@gmail.com>
2022-06-29 23:00:18 +0100

    add boat dispenser behaviour
    (fixes #9)

--- src/main/java/net/moddingplayground/twigs/impl/item/TwigsItemsImpl.java
+++ src/main/java/net/moddingplayground/twigs/impl/item/TwigsItemsImpl.java
@@ -3,9 +3,9 @@ package net.moddingplayground.twigs.impl.item;
 import net.fabricmc.api.ModInitializer;
 import net.fabricmc.fabric.api.registry.CompostingChanceRegistry;
 import net.fabricmc.fabric.api.registry.FuelRegistry;
-import net.minecraft.block.Blocks;
-import net.moddingplayground.frame.api.loottables.v0.LootTableAdditions;
-import net.moddingplayground.twigs.api.Twigs;
+import net.minecraft.block.DispenserBlock;
+import net.minecraft.block.dispenser.BoatDispenserBehavior;
+import net.moddingplayground.twigs.api.entity.TwigsBoatTypes;
 import net.moddingplayground.twigs.api.item.TwigsItems;
 
 public final class TwigsItemsImpl implements TwigsItems, ModInitializer {
@@ -20,6 +20,7 @@ public final class TwigsItemsImpl implements TwigsItems, ModInitializer {
         composting.add(BAMBOO_LEAVES, 0.5F);
         composting.add(TWIG, 0.3F);
 
-        LootTableAdditions.of(Blocks.BAMBOO).defaulted(Twigs.MOD_ID).register();
+        DispenserBlock.registerBehavior(STRIPPED_BAMBOO_BOAT, new BoatDispenserBehavior(TwigsBoatTypes.STRIPPED_BAMBOO));
+        DispenserBlock.registerBehavior(STRIPPED_BAMBOO_CHEST_BOAT, new BoatDispenserBehavior(TwigsBoatTypes.STRIPPED_BAMBOO, true));
     }
 }
